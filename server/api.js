515bafc8751deaf659d2c49c9e3959f74e3608fc
vi.tt <ttvicse@gmail.com>
2017-05-10 22:11:30 +0700

    [Auth] bug in account model can not not save profile.id

--- server/api.js
+++ server/api.js
@@ -97,12 +97,28 @@ module.exports = function(wagner) {
         status(status.UNAUTHORIZED).
         json({error: 'Not logged in'});
     }
-
     req.user.populate({ path: 'data.cart.product', model: 'Product' }, handleOne.bind(null, 'user', res));
   });
 
   return api;
 };
+
+function handleOne(property, res, error, result) {
+  if (error) {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({ error: error.toString() });
+  }
+  if (!result) {
+    return res.
+      status(status.NOT_FOUND).
+      json({ error: 'Not found' });
+  }
+
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
 
 function handleMany(property, res, error, result) {
   if (error) {
